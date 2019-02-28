3471b41fe36ceb585ec6ce57c3e47004a29ef2fb
Rafael Ostertag <rafi@guengel.ch>
2019-03-01 00:26:53 +0100

    Code beautification

--- cpp/array.cpp
+++ cpp/array.cpp
@@ -1,6 +1,6 @@
-#include <memory>
 #include <chrono>
 #include <iostream>
+#include <memory>
 
 struct StructA {
     int valueA;
@@ -9,7 +9,7 @@ struct StructA {
 };
 
 class Array {
-    public:
+   public:
     Array() : structArray{new StructA[NUMBER_OF_ELEMENTS]} {}
     Array(const Array&) = delete;
     Array(Array&&) = delete;
@@ -18,54 +18,49 @@ class Array {
     long sumUpForward() const;
     long sumUpBackward() const;
 
-    private:
+   private:
     static constexpr int NUMBER_OF_ELEMENTS{1000000};
     std::unique_ptr<StructA[]> structArray;
 };
 
 void Array::fillArray() {
-    for(auto i=0; i<NUMBER_OF_ELEMENTS; i++) {
-        structArray[i].valueA=i;
-        structArray[i].valueB=i+1;
-        structArray[i].valueC=i+2;
+    for (auto i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+        structArray[i].valueA = i;
+        structArray[i].valueB = i + 1;
+        structArray[i].valueC = i + 2;
     }
 }
 
 long Array::sumUpForward() const {
     auto sum = 0l;
-    for(auto i=0; i<NUMBER_OF_ELEMENTS; i++) {
-        sum += structArray[i].valueA + 
-                structArray[i].valueB +
-                structArray[i].valueC;
+    for (auto i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+        sum += structArray[i].valueA + structArray[i].valueB +
+               structArray[i].valueC;
     }
     return sum;
 }
 
 long Array::sumUpBackward() const {
     auto sum = 0l;
-    for(auto i=NUMBER_OF_ELEMENTS-1; i>=0; i--) {
-        sum += structArray[i].valueA + 
-                structArray[i].valueB +
-                structArray[i].valueC;
+    for (auto i = NUMBER_OF_ELEMENTS - 1; i >= 0; i--) {
+        sum += structArray[i].valueA + structArray[i].valueB +
+               structArray[i].valueC;
     }
     return sum;
 }
 
-void printWithNanos(const std::string& label, 
-                    const std::chrono::high_resolution_clock::time_point& t0, 
+void printWithNanos(const std::string& label,
+                    const std::chrono::high_resolution_clock::time_point& t0,
                     const std::chrono::high_resolution_clock::time_point& t1) {
-    std::cout << 
-        label << 
-        " took " <<
-        std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count()
-        << "µs"
-        << std::endl;
+    std::cout << label << " took "
+              << std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0)
+                     .count()
+              << "µs" << std::endl;
 }
 
 inline std::chrono::high_resolution_clock::time_point takeTime() {
     return std::chrono::high_resolution_clock::now();
 }
-
 
 int main(int, char**) {
     Array array;
