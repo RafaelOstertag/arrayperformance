caaa45fdded20e12b3a25a48db9d07f692252f43
Rafael Ostertag <rafi@guengel.ch>
2019-03-01 00:00:10 +0100

    Add log file support

---
+++ java/src/main/java/ch/guengel/LogFile.java
@@ -0,0 +1,35 @@
+package ch.guengel;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class LogFile implements AutoCloseable{
+    private final BufferedWriter logWriter;
+
+    LogFile(String logfile) {
+        try {
+            logWriter = new BufferedWriter(new FileWriter(logfile));
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open file " + logfile, e);
+        }
+    }
+
+    void write(String label, int iteration, long durationNs) {
+        try {
+            logWriter.write("java-app;" + label + ";" + iteration + ";" + durationNs);
+            logWriter.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write to log file", e);
+        }
+    }
+
+    @Override
+    public void close()  {
+        try {
+            logWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot close file", e);
+        }
+    }
+}
