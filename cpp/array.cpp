d5e8464687f0f7e6dcbd6e830000783127de145d
Rafael Ostertag <rafi@guengel.ch>
2019-03-01 01:59:55 +0100

    Log and use refs

--- cpp/array.cpp
+++ cpp/array.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 #include <memory>
 
+#include "logfile.hh"
+
+namespace {
+LogFile logfile("cpp-results.csv");
+constexpr int ITERATIONS{10000};
+}  // namespace
+
 struct StructA {
     int valueA;
     int valueB;
@@ -11,51 +18,42 @@ struct StructA {
 class Array {
    public:
     Array() : structArray{new StructA[NUMBER_OF_ELEMENTS]} {}
+    ~Array() { delete[] structArray; }
     Array(const Array&) = delete;
     Array(Array&&) = delete;
 
     void fillArray();
     long sumUpForward() const;
-    long sumUpBackward() const;
 
    private:
     static constexpr int NUMBER_OF_ELEMENTS{1000000};
-    std::unique_ptr<StructA[]> structArray;
+    StructA* structArray;
 };
 
 void Array::fillArray() {
     for (auto i = 0; i < NUMBER_OF_ELEMENTS; i++) {
-        structArray[i].valueA = i;
-        structArray[i].valueB = i + 1;
-        structArray[i].valueC = i + 2;
+        StructA& ref = structArray[i];
+        ref.valueA = i;
+        ref.valueB = i + 1;
+        ref.valueC = i + 2;
     }
 }
 
 long Array::sumUpForward() const {
     auto sum = 0l;
     for (auto i = 0; i < NUMBER_OF_ELEMENTS; i++) {
-        sum += structArray[i].valueA + structArray[i].valueB +
-               structArray[i].valueC;
+        StructA& ref = structArray[i];
+        sum += ref.valueA + ref.valueB + ref.valueC;
     }
     return sum;
 }
 
-long Array::sumUpBackward() const {
-    auto sum = 0l;
-    for (auto i = NUMBER_OF_ELEMENTS - 1; i >= 0; i--) {
-        sum += structArray[i].valueA + structArray[i].valueB +
-               structArray[i].valueC;
-    }
-    return sum;
-}
-
-void printWithNanos(const std::string& label,
-                    const std::chrono::high_resolution_clock::time_point& t0,
-                    const std::chrono::high_resolution_clock::time_point& t1) {
-    std::cout << label << " took "
-              << std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0)
-                     .count()
-              << "µs" << std::endl;
+void log(const std::string& label, int iteration,
+         const std::chrono::high_resolution_clock::time_point& t0,
+         const std::chrono::high_resolution_clock::time_point& t1) {
+    auto duration =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
+    logfile.write(label, iteration, duration);
 }
 
 inline std::chrono::high_resolution_clock::time_point takeTime() {
@@ -68,11 +66,14 @@ int main(int, char**) {
     auto t0 = takeTime();
     array.fillArray();
     auto t1 = takeTime();
-    printWithNanos("Filling array", t0, t1);
+    log("fillArray", 0, t0, t1);
 
-    t0 = takeTime();
-    auto result = array.sumUpForward();
-    t1 = takeTime();
-    printWithNanos("Summing up forward", t0, t1);
-    std::cout << "Result forward " << result << std::endl;
+    long result = -1;
+    for (auto i = 0; i < ITERATIONS; i++) {
+        t0 = takeTime();
+        result = array.sumUpForward();
+        t1 = takeTime();
+        log("sumUpForward", i, t0, t1);
+    }
+    std::cout << "Result " << result << std::endl;
 }
